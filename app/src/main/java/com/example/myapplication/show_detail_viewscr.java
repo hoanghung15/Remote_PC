0e5a621f1b3636cf14da8dafa894deaecd35429c
hoanghung15 <hungaovai15122003@gmail.com>
2024-09-15 22:22:32 +0700

    second commit

--- app/src/main/java/com/example/myapplication/show_detail_viewscr.java
+++ app/src/main/java/com/example/myapplication/show_detail_viewscr.java
@@ -5,14 +5,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -27,9 +30,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.devadvance.circularseekbar.CircularSeekBar;
 import com.github.chrisbanes.photoview.PhotoView;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class show_detail_viewscr extends AppCompatActivity {
     private PhotoView photoView;
@@ -48,11 +60,17 @@ public class show_detail_viewscr extends AppCompatActivity {
 
     private Handler handler = new Handler(Looper.getMainLooper());
 
-    private boolean firstView = true;
+    private boolean firstView = false;
 
     private ImageView upButton, downButton, leftButton, rightButton;
     private Button btnLeftClickMouse,btnRightClickMouse;
     private ImageView btnReturnDetailView;
+    //speaker
+    private TextView txtValueofSpeaker;
+    private CircularSeekBar circularSeekBar1;
+    //rotate
+    private ImageView btnRotate;
+    private boolean isLandscape = false;
 
 
     @Override
@@ -73,6 +91,7 @@ public class show_detail_viewscr extends AppCompatActivity {
         btnKeyBoardGray = findViewById(R.id.btnKeyBoardGray);
         txtQuality = findViewById(R.id.txtQuality);
         btnSpeaker = findViewById(R.id.btnSpeaker);
+        btnRotate = findViewById(R.id.btnRotate);
 
         mouseControlView = getLayoutInflater().inflate(R.layout.control_mouse, null);
         speakerControlView = getLayoutInflater().inflate(R.layout.control_speaker,null);
@@ -81,13 +100,206 @@ public class show_detail_viewscr extends AppCompatActivity {
         IntentFilter filter = new IntentFilter("UPDATE_SCREEN_IMAGE");
         registerReceiver(screenUpdateReceiver, filter);
 
+        //getVl
+        txtValueofSpeaker = speakerControlView.findViewById(R.id.txtValueofSpeaker);
+        circularSeekBar1  = speakerControlView.findViewById(R.id.circularSeekBar1);
+
         btnReturnDetailView.setOnClickListener(v -> {
             finish();
+        });
+
+        btnRotate.setOnClickListener(v -> {
+            if (isLandscape) {
+                // Trở lại trạng thái ban đầu - dọc
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                // Khôi phục kích thước ban đầu của photoView
+                ViewGroup.LayoutParams params = photoView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = 300; // Kích thước ban đầu
+                photoView.setLayoutParams(params);
+
+                // Thay đổi kiểu hiển thị
+                photoView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                isLandscape = true;
+            } else {
+                // Xoay sang trạng thái ngang
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                // Chỉnh kích thước để lấp đầy màn hình
+                ViewGroup.LayoutParams params = photoView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                photoView.setLayoutParams(params);
+
+                // Thay đổi kiểu hiển thị để lấp đầy màn hình ngang
+                photoView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                isLandscape = true;
+            }
+        });
+        btnMouse.setOnClickListener(v->{
+            showControlView(mouseControlView);
+            showControl.removeView(mouseControlView);
+            btnMouse.setImageResource(R.drawable.bntmdrk);
+            showControl.removeView(speakerControlView);
+            joystick = mouseControlView.findViewById(R.id.joystick);
+            upButton = mouseControlView.findViewById(R.id.btnUpmouse);
+            downButton = mouseControlView.findViewById(R.id.btnDownMouse);
+            leftButton = mouseControlView.findViewById(R.id.btnLeftMouse);
+            rightButton = mouseControlView.findViewById(R.id.btnRightMouse);
+            btnLeftClickMouse = mouseControlView.findViewById(R.id.btnLeftClickMouse);
+            btnRightClickMouse = mouseControlView.findViewById(R.id.btnRightClickMouse);
+
+            // Lưu vị trí ban đầu của joystick
+            joystick.post(() -> {
+                initialX = joystick.getX();
+                initialY = joystick.getY();
+            });
+
+            joystick.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    // Lấy tọa độ của frmMoveMouse để giới hạn joystick
+                    FrameLayout frmMoveMouse = findViewById(R.id.frmMoveMouse);
+                    int parentWidth = frmMoveMouse.getWidth();
+                    int parentHeight = frmMoveMouse.getHeight();
+                    int joystickWidth = joystick.getWidth();
+                    int joystickHeight = joystick.getHeight();
+
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            dX = view.getX() - motionEvent.getRawX();
+                            dY = view.getY() - motionEvent.getRawY();
+                            break;
+
+                        case MotionEvent.ACTION_MOVE:
+                            float newX = motionEvent.getRawX() + dX;
+                            float newY = motionEvent.getRawY() + dY;
+
+                            // Giới hạn X không vượt quá biên trái và phải
+                            if (newX < 0) {
+                                newX = 0;
+                            } else if (newX + joystickWidth > parentWidth) {
+                                newX = parentWidth - joystickWidth;
+                            }
+
+                            // Giới hạn Y không vượt quá biên trên và dưới
+                            if (newY < 0) {
+                                newY = 0;
+                            } else if (newY + joystickHeight > parentHeight) {
+                                newY = parentHeight - joystickHeight;
+                            }
+
+                            view.animate()
+                                    .x(newX)
+                                    .y(newY)
+                                    .setDuration(0)
+                                    .start();
+
+                            checkCollisionWithButtons(view);
+                            break;
+
+                        case MotionEvent.ACTION_UP:
+                            view.animate()
+                                    .x(initialX)
+                                    .y(initialY)
+                                    .setDuration(500)
+                                    .start();
+                            break;
+
+                        default:
+                            return false;
+                    }
+                    return true;
+                }
+            });
+
+            upButton.setOnClickListener(view->{
+                upButton.setImageResource(R.drawable.clickedmouse);
+                new Thread(() -> sendMouseCommand("MOVE_UP")).start();
+                handler.postDelayed(() -> upButton.setImageResource(R.drawable.btnumouse), 200);
+            });
+            downButton.setOnClickListener(view->{
+                downButton.setImageResource(R.drawable.clickedownmouse);
+                new Thread(() -> sendMouseCommand("MOVE_DOWN")).start();
+                handler.postDelayed(() -> downButton.setImageResource(R.drawable.btndmouse), 200);
+            });
+            leftButton.setOnClickListener(view->{
+                leftButton.setImageResource(R.drawable.clickedleftmouse);
+                new Thread(() -> sendMouseCommand("MOVE_LEFT")).start();
+                handler.postDelayed(() -> leftButton.setImageResource(R.drawable.btnlmouse), 200);
+            });
+            rightButton.setOnClickListener(view->{
+                rightButton.setImageResource(R.drawable.clickedrightmouse);
+                new Thread(() -> sendMouseCommand("MOVE_RIGHT")).start();
+                handler.postDelayed(() -> rightButton.setImageResource(R.drawable.btnrmouse), 200);
+            });
+
+            btnLeftClickMouse.setOnClickListener(view -> {
+                new Thread(() ->sendMouseCommand("LEFT_CLICK")).start();
+            });
+            btnRightClickMouse.setOnClickListener(view -> {
+                new Thread(() -> sendMouseCommand("RIGHT_CLICK")).start();
+            });
+
+            showControlView(mouseControlView);
+            btnMouse.setImageResource(R.drawable.bntmdrk);
+
+
         });
         txtQuality.setOnClickListener( v ->{
             Toast.makeText(this," Highest quality",Toast.LENGTH_SHORT).show();
         });
+        btnSpeaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showControlView(speakerControlView);
+                // Start a new thread to handle network communication
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket("192.168.1.16", 12345);
+                            // Read the message from the server
+                            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            final String message = input.readLine();
+                            // Close the socket
+                            socket.close();
+                            int test = Integer.parseInt(message);
+                            String value = message.toString() +"%";
+                            // Update the UI on the main thread
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    txtValueofSpeaker.setText(value);
+                                    circularSeekBar1.setProgress(test);
+                                }
+                            });
 
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+        circularSeekBar1.setOnSeekBarChangeListener(new CircularSeekBar.OnCircularSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(CircularSeekBar circularSeekBar, int progress, boolean fromUser) {
+                String tmp = String.valueOf(progress) +"%";
+                txtValueofSpeaker.setText(tmp);
+            }
+
+            @Override
+            public void onStopTrackingTouch(CircularSeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(CircularSeekBar seekBar) {
+
+            }
+        });
         if (firstView) {
             // Khởi tạo joystick và các nút điều khiển chỉ khi firstView là true
             joystick = mouseControlView.findViewById(R.id.joystick);
@@ -217,7 +429,6 @@ public class show_detail_viewscr extends AppCompatActivity {
         showControl.removeAllViews();  // Xóa tất cả các View cũ trong FrameLayout
         showControl.addView(controlView);  // Thêm View điều khiển mới
     }
-
     // Hàm kiểm tra va chạm giữa joystick và các nút
     private void checkCollisionWithButtons(View joystick) {
         if (isViewOverlapping(joystick, upButton)) {
@@ -230,7 +441,6 @@ public class show_detail_viewscr extends AppCompatActivity {
             right();
         }
     }
-
     // Hàm kiểm tra nếu hai view chồng lên nhau
     private boolean isViewOverlapping(View firstView, View secondView) {
         Rect firstRect = new Rect();
@@ -241,7 +451,6 @@ public class show_detail_viewscr extends AppCompatActivity {
 
         return Rect.intersects(firstRect, secondRect);
     }
-
     // Các hàm điều khiển
     private void up() {
         upButton.setImageResource(R.drawable.clickedmouse);
@@ -278,4 +487,9 @@ public class show_detail_viewscr extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+
+
+
 }
